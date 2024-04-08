9b03cff0675afdb1cfd744679bd9cbec064ab0b5
DariusKosk <dariuskosk@gmail.com>
2024-04-08 17:12:39 +0300

    Project setup changes

---
+++ Server/Controllers/PoordumineController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using AgileWorksProjekt.Server.Models; // Lisa vajadusel ruumid
+using System;
+using System.Collections.Generic;
+
+namespace AgileWorksProjekt.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PoordumineController : ControllerBase
+    {
+        private static List<Poordumine> poordumised = new List<Poordumine>();
+
+        [HttpGet]
+        public IEnumerable<Poordumine> Get()
+        {
+            return poordumised;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Poordumine poordumine)
+        {
+            poordumine.SisestamiseAeg = DateTime.Now;
+            poordumised.Add(poordumine);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            var poordumine = poordumised.Find(p => p.Id == id);
+            if (poordumine == null)
+            {
+                return NotFound();
+            }
+            poordumine.Lahendatud = true; 
+            return Ok();
+        }
+    }
+}
